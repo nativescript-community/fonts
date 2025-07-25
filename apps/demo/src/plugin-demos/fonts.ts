14e4fe9d28e1e08b9973bb920c4169374d05b42b
Jason Cassidy <47318351+jcassidyav@users.noreply.github.com>
2025-07-25 12:49:14 +0100

    chore: bump nx and pin dependencies  (#6)
    pin deps and latest plugin seed

--- apps/demo/src/plugin-demos/fonts.ts
+++ apps/demo/src/plugin-demos/fonts.ts
@@ -1,6 +1,5 @@
 import { Observable, EventData, Page } from '@nativescript/core';
 import { DemoSharedFonts } from '@demo/shared';
-import {} from '@nativescript-community/fonts';
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object;
--- apps/demo/webpack.config.js
+++ apps/demo/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('@nativescript/webpack');
 const { resolve } = require('path');
 const { addFontsConfigFontAwesome, addFontsConfigMDIFont, FontAwesomeFontType, addFontsConfigCustom } = require('@nativescript-community/fonts');
+
 module.exports = (env) => {
   webpack.init(env);
   webpack.useConfig('typescript');
@@ -24,8 +25,6 @@ module.exports = (env) => {
     stripCharactersFromFont: true,
   });
   addFontsConfigCustom({ pathToFont: '../../tools/assets/fonts/dripicons-v2.ttf', tokenPrefix: 'drip-', tokenScss: '../../tools/assets/fonts/dripicons.scss', stripCharactersFromFont: true });
-
   addFontsConfigCustom({ pathToFont: '../../tools/assets/fonts/icomoon/icomoon.ttf', tokenPrefix: 'icon-', tokenScss: '../../tools/assets/fonts/icomoon/variables.scss', tokenScssPrefix: '$icon-', stripCharactersFromFont: true });
-
   return webpack.resolveConfig();
 };
